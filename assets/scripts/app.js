47da6fe2bd952270a00363952e69866c16a03d64
allanolive <allan.loveforfood@gmail.com>
2019-02-19 10:48:13 -0500

    Created five event listeners
    - I created sign-up on submit event listener that triggers onSignUp
    function located on events.js
    - I created sign-in on submit event listener that triggers onSignIn
    function located on events.js
    - I created sign-out on click event listener that triggers onSignOut
    function located on events.js
    - I created change-password on submit event listener that triggers
    onChangePassword function located on events.js
    - I created create-game on click event listener that triggers
    onCreateGame function located on events.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,14 @@
-const logic = require('./logic.js')
+'use strict'
+
+const authEvents = require('./events.js')
+const gameLogic = require('./logic')
 
 $(() => {
-  $('#playAgainBtn').on('click', logic.playAgain)
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#change-password').on('submit', authEvents.onChangePassword)
+  $('#create-game').on('click', authEvents.onCreateGame)
+  $('#sign-out').on('click', authEvents.onSignOut)
+  $('#gameBoard div').on('click', gameLogic.getWinner)
+  $('#playAgainBtn').on('click', gameLogic.playAgain)
 })
