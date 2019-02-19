8e52a8661d13812db222cae191d96edf810d50b8
allanolive <allan.loveforfood@gmail.com>
2019-02-19 11:21:56 -0500

    Created all authentication events
    - created onSignIn, onSignUp, onSignOut, onChangePassword, and
    onCreateGame functions which uses getFormFields if necessary to
    transfer that data to its corresponding api module function and
    corresponding ui module function.

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,4 +1,54 @@
+'use strict'
 
-module.export = {
+const getFormFields = require('../../lib/get-form-fields.js')
+const api = require('./api.js')
+const ui = require('./ui.js')
 
+const onSignUp = function (event) {
+  event.preventDefault()
+  const data = getFormFields(this)
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  const data = getFormFields(this)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  console.log('sign out worked')
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  console.log('change password worked')
+  const data = getFormFields(this)
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const onCreateGame = function () {
+  event.preventDefault()
+  console.log('button works')
+  api.createGame()
+    .then(ui.createGameSuccess)
+    .catch(ui.createGameFailure)
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onSignOut,
+  onChangePassword,
+  onCreateGame
 }
