0ad951ea8ea88a32fa6abf13c546918218cc0d47
allanolive <allan.loveforfood@gmail.com>
2019-02-19 10:39:57 -0500

    Created API authentication using ajax
    - created signUp, signIn, signOut, changePassword, and createGame
    functions which use ajax in order to send user information to the
    API.

---
+++ assets/scripts/api.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const store = require('./store')
+const config = require('./config')
+
+const signUp = function (data) {
+  console.log(data)
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data
+  })
+}
+
+const signIn = function (data) {
+  console.log('data is...' + data)
+  console.log(data)
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data
+  })
+}
+
+const signOut = function () {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const changePassword = function (data) {
+  console.log('data is...' + data)
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const createGame = function () {
+  return $.ajax({
+    url: config.apiUrl + '/games',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {}
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePassword,
+  createGame
+}
